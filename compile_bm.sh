fc1a8015acaf0cac2055f3029db1a0f89e1f1241
Jose Antonio Abreu Alvarez <57382041+Monstermel@users.noreply.github.com>
2023-03-24 15:53:57 -0600

    Update compile_bm.sh

--- compile_bm.sh
+++ compile_bm.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-g++ Fibonacci-Parellel/bm_test.cpp -std=c++11 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o mybenchmark
+g++ bm_test.cpp -std=c++11 -isystem benchmark/include -Lbenchmark/build/src -lbenchmark -lpthread -o mybenchmark
